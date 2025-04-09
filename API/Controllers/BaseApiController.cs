2a2da26312011e5eabf983b5e7276c74b58481db
msan111 <melisasannn@gmail.com>
2025-04-09 21:12:09 +0200

    Implement authentication, JWT handling, and other improvements

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using System;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // api/users
+public class BaseApiController : ControllerBase
+{
+
+}
